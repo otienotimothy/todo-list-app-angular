4b270687cae6b469192341fa41d58be91365d23b
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-09 17:14:10 +0300

    project init

---
+++ src/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './task';
+
+const tasks: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: new Date('2022-10-02'),
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: new Date('2022-10-02'),
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: new Date('2022-10-02'),
+    reminder: false,
+  },
+];
